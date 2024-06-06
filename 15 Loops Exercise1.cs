9d289101e575cf439652d9cab56758d137c8473b
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-06 09:37:46 -0700

    completed loops

---
+++ 15 Loops Exercise1.cs
@@ -0,0 +1,62 @@
+15 Loops Exercise1
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    COnsoleWriteLine("{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i =0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i =0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
+for (int i =0; i < 5; i++)
+{
+    Console.WriteLine("{i}");
+}
